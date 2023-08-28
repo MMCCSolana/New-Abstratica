4cad901a21a940855e3d3e3675886dd391a1d063
MMCCSolana <114774208+MMCCSolana@users.noreply.github.com>
2023-08-27 22:11:43 -0500

    refresh

--- abstratica-web/src/api/collectors.js
+++ abstratica-web/src/api/collectors.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export default async function getCollectorData() {
-  // Logging the URL being used
-  console.log("URL being used:", process.env.VUE_APP_COLLECTOR_DATA_API);
-
   const { data } = await axios.get(process.env.VUE_APP_COLLECTOR_DATA_API);
   return data;
 }
